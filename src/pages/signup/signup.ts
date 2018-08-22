a08241ad54357fc847c28bea7f8bc30021776ba6
brandon <brandonjks@hotmail.com>
2018-08-21 21:46:08 -0500

    login

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -4,6 +4,7 @@ import { IonicPage, NavController, ToastController } from 'ionic-angular';
 
 import { User } from '../../providers';
 import { MainPage } from '../';
+import {Api} from '../../providers/api/api';
 
 @IonicPage()
 @Component({
@@ -14,10 +15,12 @@ export class SignupPage {
   // The account fields for the login form.
   // If you're using the username field with or without email, make
   // sure to add it to the type
-  account: { name: string, email: string, password: string } = {
-    name: 'Test Human',
-    email: 'test@example.com',
-    password: 'test'
+  account: { username: string, email: string, password: string,nombre:string,apellido:string } = {
+    username: '',
+    email: '',
+    password: '',
+    nombre:"",
+    apellido:""
   };
 
   // Our translated text strings
@@ -26,7 +29,10 @@ export class SignupPage {
   constructor(public navCtrl: NavController,
     public user: User,
     public toastCtrl: ToastController,
-    public translateService: TranslateService) {
+    public translateService: TranslateService, 
+    public ApiService: Api )
+   
+    {
 
     this.translateService.get('SIGNUP_ERROR').subscribe((value) => {
       this.signupErrorString = value;
@@ -34,20 +40,23 @@ export class SignupPage {
   }
 
   doSignup() {
-    // Attempt to login in through our User service
-    this.user.signup(this.account).subscribe((resp) => {
-      this.navCtrl.push(MainPage);
-    }, (err) => {
+    this.ApiService.post('users/signup',this.account).subscribe(
+      (registro) => {
 
-      this.navCtrl.push(MainPage);
+        let datoUsuario = {
+          username: this.account.username,
+          password: this.account.password
+        }
+        this.ApiService.post('users/login',datoUsuario).subscribe(
 
-      // Unable to sign up
-      let toast = this.toastCtrl.create({
-        message: this.signupErrorString,
-        duration: 3000,
-        position: 'top'
-      });
-      toast.present();
-    });
+          (usuario) => {
+            console.log(usuario);
+          },
+
+          (error) => { })
+      },
+      (err) => { }
+    )
+
   }
 }
